f2fe13abaa9b25a237eae302804b114ffe8c52c6
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-09-26 03:55:58 +0500

    canceling

--- src/bot.mjs
+++ src/bot.mjs
@@ -33,7 +33,7 @@ bot.use(session({
 bot.use(reTrigger(bot));
 bot.use(conversations());
 bot.callbackQuery("cancel", async ctx => {
-    await ctx.editMessageReplyMarkup({reply_markup: new InlineKeyboard()}).catch(console.error);
+    await ctx.editMessageReplyMarkup({reply_markup: new InlineKeyboard()}, globalThis.signal).catch(console.error);
     await ctx.answerCallbackQuery("Canceling...", globalThis.signal).catch(console.error);
     return collection.updateOne({key: ctx.chat.id.toString()}, {$set: {tasks: []}});
 });
