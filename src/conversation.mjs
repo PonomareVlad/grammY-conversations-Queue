50874414c1b4289292bf6a08fadd9497acf0c9f8
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-09-25 18:52:34 +0500

    Random error

--- src/conversation.mjs
+++ src/conversation.mjs
@@ -9,23 +9,20 @@
 
 export async function conversation(conversation, ctx) {
 
-    const {update_id} = ctx.update;
+    const step = async () => {
+        const {update_id} = ctx.update;
+        await conversation.external(() => ctx.reTrigger());
+        await conversation.waitUntil(
+            ctx => ctx.update.update_id === update_id,
+            () => console.debug("Update skipped:", ctx.update)
+        );
+    }
 
-    await ctx.reply("Hey !");
-    await conversation.external(() => ctx.reTrigger());
-    await conversation.waitUntil(
-        ctx => ctx.update.update_id === update_id,
-        () => console.debug("Update skipped:", ctx.update)
-    );
+    await ctx.reply("Hey !").then(step);
 
     if (Math.random() < 0.5) throw new Error("Random error");
 
-    await ctx.reply("Send something");
-    await conversation.external(() => ctx.reTrigger());
-    await conversation.waitUntil(
-        ctx => ctx.update.update_id === update_id,
-        () => console.debug("Update skipped:", ctx.update)
-    );
+    await ctx.reply("Send something").then(step);
 
     await conversation.waitFor("msg");
 
