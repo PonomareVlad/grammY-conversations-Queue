3c917e8667d5df016647625cee0fa7cfe380f42f
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-09-25 23:59:01 +0500

    Update

--- src/conversation.mjs
+++ src/conversation.mjs
@@ -5,27 +5,33 @@
  * @typedef {Conversation<BotContext>} BotConversation
  */
 
+const step = async conversation => {
+    const {update_id} = conversation.currentCtx.update;
+    await conversation.external(() => conversation.ctx.reTrigger());
+    await conversation.waitUntil(
+        ctx => ctx.update.update_id === update_id,
+        () => console.warn("Update skipped:", conversation.ctx.update)
+    );
+}
+
 export async function conversation(conversation, ctx) {
 
-    const step = async () => {
-        const {update_id} = ctx.update;
-        await conversation.external(() => ctx.reTrigger());
-        await conversation.waitUntil(
-            ctx => ctx.update.update_id === update_id,
-            () => console.warn("Update skipped:", ctx.update)
-        );
-    }
-
-    await ctx.reply("Hey !").then(step);
+    await ctx.reply("Hey !").then(() => step(conversation));
 
     if (Math.random() < 0.5) throw new Error("Random error");
 
-    await ctx.reply("Send any text").then(step);
+    await ctx.reply("Send any text").then(() => step(conversation));
 
     const text = await conversation.form.text(ctx => ctx.reply("Send any text"));
 
-    await ctx.reply("Done, your text:");
+    await ctx.reply("Nice, send any number of repeats for your text").then(() => step(conversation));
 
-    await ctx.reply(text);
+    let repeats = await conversation.form.number(ctx => ctx.reply("Send any number"));
+
+    await ctx.reply(`Your text repeated ${repeats} time(s):`).then(() => step(conversation));
+
+    while (repeats-- > 0) await ctx.reply(text).then(() => step(conversation));
+
+    await ctx.reply(`Done`).then(() => step(conversation));
 
 }
